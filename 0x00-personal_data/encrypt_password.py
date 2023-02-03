4f602689efe7e13bfefb633404b4fae82cae9171
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-03 10:27:36 +0100

    edited function argument name `password`

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,14 +7,14 @@ This module provides the function `hash_password`
 import bcrypt
 
 
-def hash_password(string: str) -> str:
+def hash_password(password: str) -> str:
     """
     This function takes in a string argument
     and returns a salted, hashed password
     """
     salt = bcrypt.gensalt()
-    string_bytes = string.encode('utf-8')
-    return bcrypt.hashpw(string_bytes, salt)
+    password_bytes = password.encode('utf-8')
+    return bcrypt.hashpw(password_bytes, salt)
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
