c85114b0959d2d2902f356a38072941f5240c579
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-06 20:54:56 +0100

    task 7 completed

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = getenv('AUTH_TYPE', 'auth')
+auth = getenv('AUTH_TYPE')
 if auth is not None:
     if auth == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@
 This module provides the class BasicAuth
 """
 
-from auth import Auth
+from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
@@ -12,4 +13,17 @@ class BasicAuth(Auth):
     A class `BasicAuth` for all Basic
     authentication operations
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        This method extracts the authentication
+        credentials from the Authorization header
+        """
+        if authorization_header is None:
+            return
+        if not isinstance(authorization_header, str):
+            return
+        if not bool(re.match(r'^Basic \w*', authorization_header)):
+            return
+        return authorization_header[6:]
